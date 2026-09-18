7e6cd19f0c8d127f18c145fb220e18b0182f1ff6
JamillyDS <jamilly24tec@gmail.com>
2026-09-18 17:51:27 -0300

    primeiro commit

---
+++ frontEnd/src/components/input.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+const Input = ({ getTarefa }) => {
+  const [tarefa, setTarefa] = useState("");
+
+  const postTarefa = async () => {
+    try {
+      const res = await fetch("http://localhost:3000/api/tarefa", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ content: tarefa }),
+      });
+
+      const data = await res.json();
+      if (!res.ok) {
+        throw new Error(data.message);
+      }
+
+      console.log(data);
+      setTarefa("");
+      getTarefa();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="mt-10">
+      <input
+        onChange={(e) => setTarefa(e.target.value)}
+        value={tarefa}
+        type="text"
+        placeholder="Insira a tarefa"
+        className="border p-2"
+      />
+      <button onClick={postTarefa} className="p-2 border ml-3 cursor-pointer">
+        Adicionar
+      </button>
+    </div>
+  );
+};
+
+export default Input;
